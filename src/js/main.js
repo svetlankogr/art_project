ac3a947467ab3db1ce3ce3aaf03c3ead592e67a1
Svitlana Synytsia <svetlankogr@gmail.com>
2023-10-13 21:16:59 +0200

    add checkTextInputs

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
 import forms from "./modules/forms";
+import mask from "./modules/mask";
+import checkTextInputs from "./modules/checkTextInputs";
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -14,4 +16,7 @@ window.addEventListener("DOMContentLoaded", () => {
   );
   sliders(".main-slider-item", "vertical");
   forms();
+  mask('[name="phone"]');
+  checkTextInputs('[name="name"]');
+  checkTextInputs('[name="message"]');
 });
